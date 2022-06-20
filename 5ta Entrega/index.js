54f4a020166586e2e1fb57ebc50fcfe2b9025531
patocarbone <88810457+patocarbone@users.noreply.github.com>
2022-06-20 00:58:34 -0300

    Add files via upload

---
+++ 5ta Entrega/index.js
@@ -0,0 +1,31 @@
+// Declaro variable centimetros
+
+let alturaCentimetros = 170
+console.log(alturaCentimetros)
+
+// Declaro variable metros
+
+let alturaMetros = 1.70
+console.log(alturaMetros)
+
+// Declaro variable peso
+
+let peso = 68.5
+console.log(peso)
+
+// Declaro altura redondeada hacia arriba
+
+let alturaRedondeada = alturaMetros.toFixed()
+console.log(alturaRedondeada)
+
+// Declaro peso redondeado hacia abajo
+
+let pesoRedondeado = Math.floor(peso)
+console.log(pesoRedondeado)
+
+// Declaro variable maximo valor
+
+// Esta ultima consigna no la entendi bien, intente buscar en el foro y demas pero no encontre informacion al respecto. Hice lo que entendi
+
+let maximoValor = `El maximo valor que se puede obtener en JavaScript + 1 es igual a ${Number.MAX_VALUE}` 
+console.log(maximoValor)
